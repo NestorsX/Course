339d663ad8859fb47da945a7b4ce36cc80d809bf
Кирилл Нестерович <nesterkalol@gmail.com>
2022-01-26 13:06:08 +0300

    Adding "TextProcessor" task

---
+++ TextProcessor/TextProcessor/FileController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System.IO;
+
+namespace TextProcessor
+{
+    public class FileController
+    {
+        private string? _filePath;
+        private readonly OpenFileDialog _openFileDialog;
+        private readonly SaveFileDialog _saveFileDialog;
+
+        public FileController()
+        {
+            _filePath = null;
+            _openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt"
+            };
+
+            _saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt"
+            };
+        }
+
+        public string? OpenFile()
+        {
+            if (_openFileDialog.ShowDialog() == true)
+            {
+                _filePath = _openFileDialog.FileName;
+                return File.ReadAllText(_filePath);
+            }
+
+            return null;
+        }
+
+        public void SaveFile(string text)
+        {
+            if (_filePath == null)
+            {
+                SaveFileAs(text);
+            }
+            else
+            {
+                File.WriteAllText(_filePath, text);
+            }
+        }
+
+        public void SaveFileAs(string text)
+        {
+            if (_saveFileDialog.ShowDialog() == true)
+            {
+                _filePath = _saveFileDialog.FileName;
+                File.WriteAllText(_filePath, text);
+            }
+        }
+    }
+}
---
+++ TextProcessor/TextProcessor/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace TextProcessor
+{
+    public partial class MainWindow : Window
+    {
+        private FileController _controller;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _controller = new FileController();
+        }
+
+        private void BtnNewFile_Click(object sender, RoutedEventArgs e)
+        {
+            workplace.Clear();
+        }
+
+        private void BtnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            string? text = _controller.OpenFile();
+            if (text != null)
+            {
+                workplace.Text = text;
+            }
+        }
+
+        private void BtnSaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.SaveFile(workplace.Text);
+        }
+
+        private void BtnSaveFileAs_Click(object sender, RoutedEventArgs e)
+        {
+            _controller.SaveFileAs(workplace.Text);
+        }
+    }
+}
