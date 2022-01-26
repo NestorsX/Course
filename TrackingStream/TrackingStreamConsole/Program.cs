148eb4bcc343caa86ba156678d5ccf187dbecf40
Кирилл Нестерович <nesterkalol@gmail.com>
2022-01-26 12:55:02 +0300

    Adding "TrackingStream" task

---
+++ TrackingStream/TrackingStreamConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace TrackingStreamConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ts = new TrackingStream(new FileStream(AppDomain.CurrentDomain.BaseDirectory + @"\file.txt", FileMode.Open)))
+            {
+                ts.Notify += DisplayProgress;
+                int count = 935;
+                byte[] array = new byte[count];
+                int data = ts.Read(array, 0, count);
+            }
+        }
+
+        private static void DisplayProgress(object sender, TrackingStreamEventArgs e)
+        {
+            Console.SetCursorPosition(0, 0);
+            int percents = Convert.ToInt32((double)e.Value / (double)e.Length * 100);
+            Console.WriteLine(new string('|', percents) + $" {percents}%");
+        }
+    }
+}
---
+++ TrackingStream/TrackingStreamConsole/TrackingStream.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace TrackingStreamConsole
+{
+    public class TrackingStream : Stream
+    {
+        public event EventHandler<TrackingStreamEventArgs> Notify;
+        private readonly Stream _stream;
+
+        public TrackingStream(Stream stream)
+        {
+            if (stream == null)
+            {
+                throw new ArgumentNullException("stream");
+            }
+
+            _stream = stream;
+        }
+        public override bool CanRead => _stream.CanRead;
+
+        public override bool CanSeek => _stream.CanSeek;
+
+        public override bool CanWrite => _stream.CanWrite;
+
+        public override long Length => _stream.Length;
+
+        public override long Position
+        {
+            get => _stream.Position;
+            set => _stream.Position = value;
+        }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (buffer == null)
+            {
+                throw new ArgumentNullException("Buffer");
+            }
+
+            if (offset < 0)
+            {
+                throw new ArgumentOutOfRangeException("Offset");
+            }
+
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("Count");
+            }
+
+            if (buffer.Length - offset < count)
+            {
+                throw new ArgumentException("Count must be less or equal to length of array");
+            }
+
+            int part = count / 10;
+            int value = 0;
+            while (count != 0 && value != Length)
+            {
+                if (part > count)
+                {
+                    part = count;
+                }
+
+                value += _stream.Read(buffer, offset, part);
+                OnPartOfFileIsRead(new TrackingStreamEventArgs { Length = Length, Value = value });
+                offset += part;
+                count -= part;
+            }
+
+            return value;
+        }
+
+        private void OnPartOfFileIsRead(TrackingStreamEventArgs e)
+        {
+            EventHandler<TrackingStreamEventArgs> handler = Notify;
+            Thread.Sleep(500);
+            handler?.Invoke(this, e);
+        }
+
+        public override void Close()
+        {
+            _stream.Close();
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _stream.Write(buffer, offset, count);
+        }
+    }
+
+    public class TrackingStreamEventArgs : EventArgs
+    {
+        public long Length { get; set; }
+        public int Value { get; set; }
+    }
+}
